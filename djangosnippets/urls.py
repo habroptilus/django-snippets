bb20769a96a9b3edc3353e16ae243e5d01bbf6d1
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2021-08-30 10:12:41 +0900

    routing

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from snippets.views import top
 
 urlpatterns = [
+    path("", top, name="top"),  # ルートのスラッシュは省略する
+    path("snippets/", include("snippets.urls")),  # snippets/urls.pyのルーティングを展開
     path('admin/', admin.site.urls),
 ]
