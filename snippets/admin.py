31fc70d87f3131328d6ce49711d4ae4720e12d9c
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2021-09-04 10:39:13 +0900

    add model

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from snippets.models import Snippet
 # Register your models here.
+
+# 管理画面で利用するためにSnippetクラスを追加
+# これでSnippetテーブルを管理画面からいじることができる
+
+admin.site.register(Snippet)
